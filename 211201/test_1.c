4816cf71fa28acc628bf2f348f12f43dc4d5b79e
Soohyeon02 <tngusr@naver.com>
2021-12-01 23:55:20 +0900

    The end :)

---
+++ 211201/test_1.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//int main(void)
+//{
+//	int arr[5];
+//	int i = 0;
+//	for (i = 0; i < 5; ++i);
+//	{
+//		arr[i] = i + 1;
+//	}
+//	printf("%d\n", arr);
+//	printf("%d\n", &arr[0]);
+//}
